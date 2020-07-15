e4243484702082c354f4137c1f9a9a2427173b2a
Darryl.Mendonez <darryl.mendonez@Laptop.home>
2020-07-14 22:11:05 -0400

    Updates

---
+++ precache-manifest.45b6b5f883a260a1bcbd48dedc6f6a57.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb96a8df1a6fb2bf6a7878465c50944",
+    "url": "/misfits-market/index.html"
+  },
+  {
+    "revision": "ce1935eddcb63609700f",
+    "url": "/misfits-market/static/css/main.42d4eeed.chunk.css"
+  },
+  {
+    "revision": "59b29244fe10971f6cc1",
+    "url": "/misfits-market/static/js/2.9b3cca49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/misfits-market/static/js/2.9b3cca49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce1935eddcb63609700f",
+    "url": "/misfits-market/static/js/main.cf80c990.chunk.js"
+  },
+  {
+    "revision": "cdedb1afe830fe126f8c",
+    "url": "/misfits-market/static/js/runtime-main.a06bd151.js"
+  },
+  {
+    "revision": "7616eabe75113aa5049aaf769fbd95f1",
+    "url": "/misfits-market/static/media/beets.7616eabe.jpg"
+  },
+  {
+    "revision": "34c00947719154b66ae12836ccca690f",
+    "url": "/misfits-market/static/media/butternut-squash.34c00947.jpg"
+  },
+  {
+    "revision": "e3c1d2a1198daa42f0af6579f7020613",
+    "url": "/misfits-market/static/media/canola-oil.e3c1d2a1.jpg"
+  },
+  {
+    "revision": "371a8bd6edbfef2760472607c3caea8b",
+    "url": "/misfits-market/static/media/green-bell-peppers.371a8bd6.jpg"
+  },
+  {
+    "revision": "9778459df5cef99b0d1eec9a1c0202e9",
+    "url": "/misfits-market/static/media/guacamole.9778459d.jpg"
+  },
+  {
+    "revision": "4baae736085da0496b871391f382ec12",
+    "url": "/misfits-market/static/media/hen-of-the-wood.4baae736.jpg"
+  },
+  {
+    "revision": "86fbb50c39b6d873f87c05c4e8f16dfb",
+    "url": "/misfits-market/static/media/mushrooms.86fbb50c.jpg"
+  },
+  {
+    "revision": "a996c31f36fbaaad91f3a24dba7a8907",
+    "url": "/misfits-market/static/media/pears.a996c31f.jpg"
+  },
+  {
+    "revision": "db2309a68853eff42e7de9554b30aa41",
+    "url": "/misfits-market/static/media/pineapple.db2309a6.jpg"
+  },
+  {
+    "revision": "cfe37dbb4bb4a896ddb728c700e21e3d",
+    "url": "/misfits-market/static/media/pomelo.cfe37dbb.jpg"
+  },
+  {
+    "revision": "765164a00915ab5eb3023118d55631a4",
+    "url": "/misfits-market/static/media/rosemary.765164a0.jpg"
+  }
+]);
